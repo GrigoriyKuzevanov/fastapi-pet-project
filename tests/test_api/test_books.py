6dad665cadba540c6e4313ffccb66a414db03351
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-16 14:25:01 +0500

    add tests for create books.

--- tests/test_api/test_books.py
+++ tests/test_api/test_books.py
@@ -39,3 +39,99 @@ def test_get_book(
     assert book.description == test_books[book_id - 1].description
     assert book.author_id == test_books[book_id - 1].author_id
     assert book.owner_id == test_books[book_id - 1].owner_id
+
+
+@pytest.mark.parametrize(
+    "title, genre, language, publish_date, description",
+    [
+        (
+            "test_title1",
+            "test_genre1",
+            "test_language1",
+            "1901-01-12",
+            "test_decription1",
+        ),
+        (
+            "test_title2",
+            "test_genre2",
+            "test_language2",
+            "1902-02-11",
+            "test_decription2",
+        ),
+        (
+            "test_title3",
+            "test_genre3",
+            "test_language3",
+            "1903-03-10",
+            "test_decription3",
+        ),
+    ],
+)
+def test_create_book(
+    authorized_client: TestClient,
+    test_authors: list[models.Author],
+    title: str,
+    genre: str,
+    language: str,
+    publish_date: str,
+    description: str,
+) -> None:
+    book_data = {
+        "title": title,
+        "genre": genre,
+        "language": language,
+        "publish_date": publish_date,
+        "description": description,
+        "author_id": 1,
+    }
+    book_to_post = schemas.BookCreate(**book_data)
+
+    response = authorized_client.post("/books/", json=book_data)
+
+    assert response.status_code == 201
+
+    created_book = schemas.BookOut(**response.json())
+
+    assert created_book.id == 1
+    assert created_book.title == book_to_post.title
+    assert created_book.genre == book_to_post.genre
+    assert created_book.language == book_to_post.language
+    assert created_book.publish_date == book_to_post.publish_date
+    assert created_book.description == book_to_post.description
+    assert created_book.author_id == book_to_post.author_id
+
+
+def test_create_book_unauthorized(
+    client: TestClient, test_authors: list[models.Author]
+) -> None:
+    book_data = {
+        "title": "test_title",
+        "genre": "test_genre",
+        "language": "test_language",
+        "publish_date": "1800-01-01",
+        "description": "test_description",
+        "author_id": 2,
+    }
+
+    response = client.post("/books/", json=book_data)
+
+    assert response.status_code == 401
+
+
+def test_create_book_author_not_exists(authorized_client: TestClient) -> None:
+    author_id = 1111
+    book_data = {
+        "title": "test_title",
+        "genre": "test_genre",
+        "language": "test_language",
+        "publish_date": "1800-01-01",
+        "description": "test_description",
+        "author_id": author_id,
+    }
+
+    response = authorized_client.post("/books/", json=book_data)
+
+    assert response.status_code == 404
+    assert (
+        response.json().get("detail") == f"Author with id: {author_id} does not exist"
+    )
