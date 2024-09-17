338da722ed7407210c7434c4a53ff065a40d56ff
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-17 12:10:11 +0500

    add tests for create users.

--- tests/test_api/test_users.py
+++ tests/test_api/test_users.py
@@ -36,3 +36,42 @@ def test_get_user(client: TestClient, test_user: dict) -> None:
     assert user.created_at == users_utils.string_to_datetime(
         test_user.get("created_at")
     )
+
+
+@pytest.mark.parametrize(
+    "password, email",
+    [
+        ("test_password1", "test_email1@email.com"),
+        ("test_password2", "test_email2@mail.com"),
+        ("test_password3", "test_email3@mail.com"),
+    ],
+)
+def test_create_user(client: TestClient, password: str, email: str) -> None:
+    user_data = {
+        "password": password,
+        "email": email,
+    }
+
+    response = client.post("/users/", json=user_data)
+
+    assert response.status_code == 201
+
+    user = schemas.UserOut(**response.json())
+
+    assert user.email == user_data.get("email")
+
+
+def test_create_user_email_exists(client: TestClient, test_user: dict) -> None:
+    existing_email = test_user.get("email")
+    user_data = {
+        "password": "secret",
+        "email": existing_email,
+    }
+
+    response = client.post("/users/", json=user_data)
+
+    assert response.status_code == 409
+    assert (
+        response.json().get("detail")
+        == f"User with email: {existing_email} already exists!"
+    )
