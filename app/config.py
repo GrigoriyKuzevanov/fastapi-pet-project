652f9d07c2feb9db9e07bee21824b54ec20cf524
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-23 10:15:46 +0500

    create create postgres url settings method.

--- app/config.py
+++ app/config.py
@@ -1,12 +1,14 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Annotated
-from pydantic import BeforeValidator
+
+from pydantic import BeforeValidator, PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 def parse_cors_origins(variable: str) -> list[str]:
     if isinstance(variable, str):
         return [item.strip() for item in variable.split(",")]
-    
+
     raise ValueError(variable)
 
 
@@ -14,9 +16,22 @@ class Settings(BaseSettings):
     db_user: str
     db_password: str
     db_host: str
-    db_port: str
+    db_port: int
     db_name: str
     echo_sql: bool
+
+    @computed_field
+    @property
+    def postgres_url(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql",
+            username=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port,
+            path=self.db_name,
+        )
+
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
