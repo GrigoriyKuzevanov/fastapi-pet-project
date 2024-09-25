4019974a525a3ffa888e23e499afe829b1a2dbbf
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-25 12:04:14 +0500

    add logging to create admin script.

--- app/scripts/create_first_admin.py
+++ app/scripts/create_first_admin.py
@@ -1,23 +1,40 @@
+import logging
+
 from sqlalchemy.orm import Session
 
 from app import models, utils
 from app.core.config import settings
 from app.core.database import SessionLocal
+from app.routers import crud
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def create_first_admin(session: Session) -> None:
-    hashed_password = utils.hash(settings.admin_password)
+    try:
+        db_user = crud.read_user_by_email(session=session, email=settings.admin_email)
+        if not db_user:
+            hashed_password = utils.hash(settings.admin_password)
 
-    admin_data = {
-        "email": settings.admin_email,
-        "password": hashed_password,
-        "is_admin": True,
-    }
+            admin_data = {
+                "email": settings.admin_email,
+                "password": hashed_password,
+                "is_admin": True,
+            }
 
-    admin = models.User(**admin_data)
+            admin = models.User(**admin_data)
 
-    session.add(admin)
-    session.commit()
+            session.add(admin)
+            session.commit()
+
+            logger.info(f"Admin is created")
+            
+        else:
+            logger.info(f"User with email: {settings.admin_email} already exists!")
+            
+    except Exception as error:
+        logger.error(f"Error while creating admin: {error}")
 
 
 if __name__ == "__main__":
