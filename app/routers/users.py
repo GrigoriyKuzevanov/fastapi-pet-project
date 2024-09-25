e1dfe3efb33ddf567ec8e75651d4aaf4a3b7c372
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-25 13:45:17 +0500

    add users update for admin.

--- app/routers/users.py
+++ app/routers/users.py
@@ -73,6 +73,34 @@ def delete_user_me(
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+@router.put(
+    "/{user_id}", status_code=status.HTTP_200_OK, response_model=schemas.UserOut
+)
+def update_user(
+    user_id: int,
+    user: schemas.UserUpdate,
+    session: Session = Depends(database.get_db),
+    current_admin: models.User = Depends(oauth2.get_current_admin),
+):
+    db_user_by_id = crud.read_user_by_id(session=session, user_id=user_id)
+
+    if not db_user_by_id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id: {user_id} does not exist",
+        )
+
+    db_user_by_email = crud.read_user_by_email(session=session, email=user.email)
+
+    if db_user_by_email:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"User with email: {user.email} already exists!",
+        )
+
+    return crud.update_user(session=session, db_user=db_user_by_id, update_data=user)
+
+
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(
     user_id: int,
